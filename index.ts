9a1dcae88e3dac1d53048ab129e92caeabca43a2
Jorge Matos <jisapjisap@gmail.com>
2023-02-03 13:25:49 +0100

    Primer commit

---
+++ index.ts
@@ -0,0 +1,71 @@
+import { prisma } from './db.js';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { gql } from 'graphql-tag';
+
+//index.ts -> index.js
+//db.ts -> db.js
+
+
+( async function(){
+
+    // .gql
+    const typeDefs = gql`
+        type Post {
+            id: String
+            title: String
+            username: String
+        }
+
+        type Query {
+            getAllPost: [Post]
+        }
+
+        type Mutation {
+            createPost( title:String, username: String): Post
+        }
+    `
+
+    interface createPostInput{
+        title: string
+        username: string
+    }
+
+    const resolvers = {
+        Mutation:{
+            createPost: async( _parent: any, args: createPostInput ) => {
+                const post = await prisma.post.create({
+                    data:{
+                        title: args.title,
+                        username: args.username
+
+                    }
+                });
+
+                return post;
+            }
+        },
+        Query: {
+            getAllPost: async () => {
+                return await prisma.post.findMany();
+            }
+        }
+    }
+
+
+    // Graphql Types vs Prisma Models
+    // Post -> id, title, username
+    // Prisma -> all the data being saved to your database
+
+    const server = new ApolloServer({
+
+        typeDefs,
+        resolvers
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 }
+    });
+
+    console.log("Server is ready at " + url)
+})();
